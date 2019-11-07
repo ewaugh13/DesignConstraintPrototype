8b7289586c77975466f0ae46a7bb717215b9727c
Manas Rawat <manas.rawat@utah.edu>
2019-11-06 17:08:02 -0700

    Updated basics of the DamagePlayer script

--- DesignConstraintPrototype/Assets/Scripts/DamagePlayer.cs
+++ DesignConstraintPrototype/Assets/Scripts/DamagePlayer.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DamagePlayer : MonoBehaviour
 {
+    private string ObstacleArea = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,31 @@ public class DamagePlayer : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // If the player enters a trigger, the player resets
+    private void OnTriggerEnter(Collider other)
+    {
+        if( other.gameObject.tag.Equals("Player") )
+        {
+            // get the current position of the player
+            ObstacleArea = this.gameObject.tag;
+
+            if( ObstacleArea == "Area1" )
+            {
+                // Move the player to area 1 start
+            }
+
+            if ( ObstacleArea == "Area2" )
+            {
+                // Move the player to area 1 start
+            }
+
+            if (ObstacleArea == "Area3")
+            {
+                // Move the player to area 3 start
+            }
+
+        }
     }
 }

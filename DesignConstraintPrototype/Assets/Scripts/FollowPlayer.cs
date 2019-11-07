1300376496961abfdcf082e99a1e3c7819b5c2df
dhawthorn <d.s.hawthorn@gmail.com>
2019-11-07 11:16:48 -0700

    particle stuff

---
+++ DesignConstraintPrototype/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    #region Instance Variables
+    [SerializeField]
+    [Tooltip("The GameObject to follow")]
+    private GameObject FollowObject = null;
+    #endregion
+
+    void Update()
+    {
+        transform.position = FollowObject.transform.position;
+    }
+}

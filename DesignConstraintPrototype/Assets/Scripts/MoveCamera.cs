699006c3f8c1692b3c76802ca046cbaac89b7879
Deep Nath <u1274435@ad.utah.edu>
2019-11-07 11:29:21 -0700

    Move Player Forward and Endless World Generation

---
+++ DesignConstraintPrototype/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    #region MemberVariables
+    private Transform target;
+    private Vector3 startOffset;
+    private Vector3 moveVector;
+
+    //private float transition = 0.0f;
+    //private float animationDuration = 2.0f;
+    //private Vector3 animationOffset = new Vector3(0, 0, -3);
+    #endregion
+
+    #region Start
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        startOffset = this.transform.position - target.position;
+    }
+    #endregion
+
+    #region Update
+    void Update()
+    {
+        moveVector = target.position + startOffset;
+
+        moveVector.x = 0; // Always centered on the track
+        moveVector.y = 0.3f; // Always centered above the track
+        //moveVector.y = Mathf.Clamp(moveVector.y, 3, 5); // If there are ups and downs
+
+        this.transform.position = moveVector;
+
+        //// Required for Initial Animation
+        //if (transition > 1.0f)
+        //{
+        //    this.transform.position = moveVector;
+        //}
+        //else
+        //{
+        //    this.transform.position = Vector3.Lerp(moveVector + animationOffset, moveVector, transition);
+        //    transition += Time.deltaTime * 1 / animationDuration; // The total time of the starting animation
+        //    this.transform.LookAt(target.position + Vector3.up);
+        //}
+    }
+    #endregion
+}
---
+++ DesignConstraintPrototype/Assets/Scripts/WallManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallManager : MonoBehaviour
+{
+    #region MemberVariables
+    public GameObject[] wallPrefabs;
+    private Transform playerTransform;
+    private float spawnZ = 0.0f;
+    private float wallLength = 2.0f;
+    private int wallsOnScreen = 10;
+    private List<GameObject> activeWalls;
+    private float safeZone = 4;
+
+    private int lastWallIndex = 0;
+    #endregion
+
+    #region Start
+    void Start()
+    {
+        activeWalls = new List<GameObject>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        for(int i = 0; i < wallsOnScreen; i++)
+        {
+            if (i < 4)
+            {
+                SpawnWall(0);
+            }
+            else
+            {
+                SpawnWall();
+            }
+        }
+    }
+    #endregion
+
+    #region Update
+    void Update()
+    {
+        if((playerTransform.position.z - safeZone) > (spawnZ - (wallsOnScreen * wallLength)))
+        {
+            SpawnWall();
+            DeleteWall();
+        }
+    }
+    #endregion
+
+    #region SpawnWall
+    private void SpawnWall(int prefabIndex = -1)
+    {
+        GameObject wall;
+        if (prefabIndex == -1)
+        {
+            wall = Instantiate(wallPrefabs[RandomWallIndex()]) as GameObject;
+        }
+        else
+        {
+            wall = Instantiate(wallPrefabs[prefabIndex]);
+        }
+        wall.transform.SetParent(this.transform);
+        wall.transform.position = Vector3.forward * spawnZ;
+        spawnZ += wallLength;
+        activeWalls.Add(wall);
+    }
+    #endregion
+
+    #region DeleteWall
+    private void DeleteWall()
+    {
+        Destroy(activeWalls[0]);
+        activeWalls.RemoveAt(0);
+    }
+    #endregion
+
+    #region RandomWallIndex
+    private int RandomWallIndex()
+    {
+        if(wallPrefabs.Length <= 1)
+        {
+            return 0;
+        }
+
+        int randomIndex = lastWallIndex;
+        while(randomIndex == lastWallIndex)
+        {
+            randomIndex = Random.Range(0, wallPrefabs.Length);
+        }
+
+        lastWallIndex = randomIndex;
+        return randomIndex;
+    }
+    #endregion
+}

a1b203174be0a93dba6ca9c6777153cbf7dadc1e
SpencedOut <strength2succeed@gmail.com>
2019-11-08 22:12:24 -0700

    Death on "spike" hit

---
+++ DesignConstraintPrototype/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollision : MonoBehaviour
+{
+    #region Hidden Variables
+    private Scene sceneToLoad;
+    #endregion
+
+    private void Start()
+    {
+        sceneToLoad = SceneManager.GetActiveScene();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag.Equals("Obsticale"))
+        {
+            Destroy(this.gameObject);
+            SceneManager.LoadScene(sceneToLoad.name);
+        }
+    }
+}

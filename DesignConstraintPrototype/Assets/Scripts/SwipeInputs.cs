d7d470ccdb17b37a4c9af7d02a937e9043cab6a5
Manas Rawat <manas.rawat@utah.edu>
2019-11-11 11:34:55 -0700

    Updated the swipe script to test for the swipe input.
    This commit is just to have the test in place.. IGNORE the changes in this commit.

--- DesignConstraintPrototype/Assets/Scripts/SwipeInputs.cs
+++ DesignConstraintPrototype/Assets/Scripts/SwipeInputs.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class SwipeInputs : MonoBehaviour
 {
     #region Hidden Variables
-    private Vector3 fp;   //First touch position
-    private Vector3 lp;   //Last touch position
+    private Vector3 FirstTouchPoint;   //First touch position
+    private Vector3 LastTouchPoint;   //Last touch position
     private float dragDistance;  //minimum distance for a swipe to be registered
+
+    private GameObject SwipeRightText;
+    private GameObject SwipeLeftText;
     #endregion
 
 
@@ -18,6 +21,13 @@ public class SwipeInputs : MonoBehaviour
     void Start()
     {
         dragDistance = Screen.height * 15 / 100; //dragDistance is 15% height of the screen
+        SwipeLeftText = GameObject.Find("Swipe Left");
+        Debug.Log(SwipeLeftText);
+        SwipeRightText = GameObject.Find("Swipe Right");
+        Debug.Log(SwipeRightText);
+        SwipeLeftText.SetActive(false);
+        SwipeRightText.SetActive(false);
+        Debug.developerConsoleVisible = true;
     }
 
     void Update()
@@ -27,35 +37,37 @@ public class SwipeInputs : MonoBehaviour
             Touch touch = Input.GetTouch(0); // get the touch
             if (touch.phase == TouchPhase.Began) //check for the first touch
             {
-                fp = touch.position;
-                lp = touch.position;
+                FirstTouchPoint = touch.position;
+                LastTouchPoint = touch.position;
             }
             else if (touch.phase == TouchPhase.Moved) // update the last position based on where they moved
             {
-                lp = touch.position;
+                LastTouchPoint = touch.position;
             }
             else if (touch.phase == TouchPhase.Ended) //check if the finger is removed from the screen
             {
-                lp = touch.position;  //last touch position. Ommitted if you use list
+                LastTouchPoint = touch.position;  //last touch position. Ommitted if you use list
 
                 //Check if drag distance is greater than 20% of the screen height
-                if (Mathf.Abs(lp.x - fp.x) > dragDistance || Mathf.Abs(lp.y - fp.y) > dragDistance)
+                if (Mathf.Abs(LastTouchPoint.x - FirstTouchPoint.x) > dragDistance || Mathf.Abs(LastTouchPoint.y - FirstTouchPoint.y) > dragDistance)
                 {//It's a drag
                  //check if the drag is vertical or horizontal
-                    if (Mathf.Abs(lp.x - fp.x) > Mathf.Abs(lp.y - fp.y))
+                    if (Mathf.Abs(LastTouchPoint.x - FirstTouchPoint.x) > Mathf.Abs(LastTouchPoint.y - FirstTouchPoint.y))
                     {   //If the horizontal movement is greater than the vertical movement...
-                        if ((lp.x > fp.x))  //If the movement was to the right)
+                        if ((LastTouchPoint.x > FirstTouchPoint.x))  //If the movement was to the right)
                         {   //Right swipe
                             Debug.Log("Right Swipe");
+                            SwipeRightText.SetActive(true);
                         }
                         else
                         {   //Left swipe
                             Debug.Log("Left Swipe");
+                            SwipeLeftText.SetActive(true);
                         }
                     }
                     //else
                     //{   //the vertical movement is greater than the horizontal movement
-                    //    if (lp.y > fp.y)  //If the movement was up
+                    //    if (LastTouchPoint.y > FirstTouchPoint.y)  //If the movement was up
                     //    {   //Up swipe
                     //        Debug.Log("Up Swipe");
                     //    }

1078671f526953f82a1ab396f93fa9354dd760b4
dhawthorn <d.s.hawthorn@gmail.com>
2019-11-10 19:24:47 -0700

    Sorry not sorry. Lighting test stuff.

---
+++ DesignConstraintPrototype/Assets/Scripts/StopTouchingMyCheese.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopTouchingMyCheese : MonoBehaviour
+{
+    #region Instance Variables
+    [SerializeField]
+    [Tooltip("The GameObject to follow")]
+    private GameObject FollowObject = null;
+    [SerializeField]
+    private Vector3 Offset = Vector3.zero;
+    #endregion
+
+    private void Start()
+    {
+        transform.position = FollowObject.transform.position - Offset;
+    }
+
+    void Update()
+    {
+
+            transform.position = FollowObject.transform.position - Offset;
+        
+    }
+}

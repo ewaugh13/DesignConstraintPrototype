4dc1262bc07110b220e2f184604d2c7ec58359ee
dhawthorn <d.s.hawthorn@gmail.com>
2019-11-11 18:38:11 -0700

    Added lightmap scipt but its broko

--- DesignConstraintPrototype/Assets/Scripts/DuplicateAndMoveALittle.cs
+++ DesignConstraintPrototype/Assets/Scripts/DuplicateAndMoveALittle.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class DuplicateAndMoveALittle : MonoBehaviour
 {
+    public GameObject copyGameObject;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject newGameObject = Instantiate(copyGameObject);
+        Vector3 newPosition = copyGameObject.transform.position;
+        newGameObject.transform.position = newPosition + (new Vector3(5, 0, 0));
     }
 
     // Update is called once per frame
